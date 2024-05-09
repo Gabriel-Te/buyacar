698fff8797fbbd6f60d90d4409bd63183b2d729e
Gabriel-Te <gabrielteixeiracorrea5@gmail.com>
2024-05-09 17:52:21 -0300

    criado os componentes necessarios, inicio da flatlist com daabase ficticio e Nav feito

--- App.js
+++ App.js
@@ -1,11 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList} from 'react-native';
+import Nav from './layout/Nav';
+import Card from './components/Card';
 
 export default function App() {
+
+  const itens = [
+    {
+      id : 1,
+      name: 'Lotus Carlton',
+      price: 140000,
+      image: require('./assets/lotusCarlton.jpg')
+    },
+    {
+      id : 2,
+      name: 'Lotuas Carlton',
+      price: 140000,
+      image: require('./assets/lotusCarlton.jpg')
+    },
+    {
+      id : 3,
+      name: 'Lotus Carlton',
+      price: 140000,
+      image: require('./assets/lotusCarlton.jpg')
+    },
+    {
+      id : 4,
+      name: 'Lotus Carlton',
+      price: 140000,
+      image: require('./assets/lotusCarlton.jpg')
+    },
+  ]
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
       <StatusBar style="auto" />
+      <Nav/>
+      <Text>FlatList</Text>
+      <FlatList
+      style={styles.flatlist}
+      data={itens}
+      numColumns={2}
+      renderItem={({item}) => 
+        <Card image={item.image} name={item.name} price={item.price}/>
+      }
+      keyExtractor={(item) => item.id.toString()}
+      /> 
     </View>
   );
 }
@@ -13,8 +54,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    textAlign: 'center',
     justifyContent: 'center',
+    backgroundColor: '#a6a6a6',
   },
+  flatlist: {
+    flex: 1
+  }
 });
---
+++ components/Card.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+
+function Card({image,name,price}) {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo} source={image}/>
+      <Text style={styles.title}>{name}</Text>
+      <Text>{price}</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+    container: {
+      display: 'flex',
+      width: '100%',
+      height: 500
+    },
+    logo: {
+      width: 200,
+      height: 300
+    }
+  });
+  
+export default Card
